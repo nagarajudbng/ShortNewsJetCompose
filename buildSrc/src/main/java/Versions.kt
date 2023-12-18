176f6c95986ace752fbeca4c32b965664070bfb7
nagarajudbng <nagaraj.dbng@gmail.com>
2023-12-19 00:53:25 +0530

    try2
    try2

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,7 +3,7 @@ object Versions {
     const val hiltWork= "1.0.0"
     const val hiltAndroidCompiler= "2.48"
     const val hiltAndroid= "2.48"
-    const val coreKtx = "1.9.0"
+    const val coreKtx = "1.8.0"
     const val kotlinBom = "1.8.0"
     const val runtimeKtx="2.3.1"
     const val activityCompose="1.5.1"
